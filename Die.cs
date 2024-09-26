ab358e987177ff27ead8b45172473cc0a29f144d
superfreak58 <rayjkolyn@gmail.com>
2024-09-26 09:39:21 -0400

    Add project files.

---
+++ Die.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Topics_1___6_Die_Game
+{
+    internal class Die
+    {
+        private Random _generator;
+        private int _roll;
+
+        public Die()
+        {
+            _generator = new Random();
+            _roll = _generator.Next(1, 7);
+        }
+
+        public Die(int roll)
+        {
+            _generator = new Random();
+
+            if (roll < 1)
+                _roll = 1;
+            else if (roll > 6)
+                _roll = 6;
+            else
+                _roll = roll;
+        }
+
+        public int Roll { get { return _roll; } }
+
+        public void RollDie()
+        {
+            _roll = _generator.Next(1, 7);
+        }
+
+        public override string ToString()
+        {
+            return _roll.ToString();
+        }
+
+        public void DrawRoll()
+        {
+            switch (_roll)
+            {
+                case 1:
+                    Console.WriteLine(" ---");
+                    Console.WriteLine("|   |");
+                    Console.WriteLine("| o |");
+                    Console.WriteLine("|   |");
+                    Console.WriteLine(" ---");
+                    break;
+                case 2:
+                    Console.WriteLine(" ---");
+                    Console.WriteLine("|o  |");
+                    Console.WriteLine("|   |");
+                    Console.WriteLine("|  o|");
+                    Console.WriteLine(" ---");
+                    break;
+                case 3:
+                    Console.WriteLine(" ---");
+                    Console.WriteLine("|o  |");
+                    Console.WriteLine("| o |");
+                    Console.WriteLine("|  o|");
+                    Console.WriteLine(" ---");
+                    break;
+                case 4:
+                    Console.WriteLine(" ---");
+                    Console.WriteLine("|o o|");
+                    Console.WriteLine("|   |");
+                    Console.WriteLine("|o o|");
+                    Console.WriteLine(" ---");
+                    break;
+                case 5:
+                    Console.WriteLine(" ---");
+                    Console.WriteLine("|o o|");
+                    Console.WriteLine("| o |");
+                    Console.WriteLine("|o o|");
+                    Console.WriteLine(" ---");
+                    break;
+                case 6:
+                    Console.WriteLine(" ---");
+                    Console.WriteLine("|o o|");
+                    Console.WriteLine("|o o|");
+                    Console.WriteLine("|o o|");
+                    Console.WriteLine(" ---");
+                    break;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿namespace Topics_1___6_Die_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int bet = 1, total;
+            bool done = false;
+            bool betDone = false;
+            string choice;
+            Die die1 = new Die();
+            Die die2 = new Die();
+            total = 100;
+            while (!done)
+            {
+                Console.WriteLine("Welcome to the casino! We just opened so all we have are die games. Let's play!");
+                Console.WriteLine("Your current balance is $100");
+                Console.WriteLine("How much do you wanna bet");
+                while (!betDone)
+                {
+                    bet = Convert.ToInt32(Console.ReadLine());
+                    if (bet > 0 && bet <= total)
+                    {
+                        betDone = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("That is an invalid bet... Get your money up boy");
+                    }
+                    while (!done)
+                    {
+
+
+                        Console.WriteLine("What will you bet betting on?");
+                        Console.WriteLine("1 - Doubles");
+                        Console.WriteLine("2 - Not Doubles");
+                        Console.WriteLine("...");
+                        Console.WriteLine("3 - Even");
+                        Console.WriteLine("4 - Odd");
+                        Console.WriteLine();
+                        choice = Console.ReadLine().ToLower().Trim();
+                        Console.WriteLine();
+                    }
+                    Console.WriteLine($"Die 1 = {die1}");
+                    die1.DrawRoll();
+                    Console.WriteLine("Rolling");
+                    Thread.Sleep(750);
+                    Console.WriteLine($"Die 2 = {die2}");
+                    die2.DrawRoll();
+
+                    if (choice = "1")
+                    if  (die1.Roll == die2.Roll)
+                    {
+                        Console.WriteLine("Wow you rolled doubles!");
+                        total += bet * 2;
+                        Console.WriteLine("Your total now is " + total);
+                    }
+
+                    else if ((die1.Roll + die2.Roll) % 2 == 0)
+                    {
+                        Console.WriteLine("Even sum! Cool");
+                        total += bet;
+                        Console.WriteLine("Your total now is " + total);
+                    }
+                    else if ((die1.Roll + die2.Roll) % 2 != 0)
+                    {
+                        Console.WriteLine("Odd sum! Cool.");
+                        total += bet;
+                        Console.WriteLine("Your total now is " + total);
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Tough luck... Thanks for the money");
+                        total -= bet;
+                        Console.WriteLine("Your total now is " + total);
+                    }
+                }
+            }
+        }
+    }
+}
+    
+
+
